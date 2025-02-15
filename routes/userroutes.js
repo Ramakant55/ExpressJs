3789b86f46499f3a3d2306ae5b908399e52e88a8
Ramakant55 <jangirramakant786@gmail.com>
2025-02-15 16:32:33 +0530

    user

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -3,9 +3,13 @@ const User = require("../models/user");
 const {sendEmail}=require("../config/emailConfig");
 const jwt=require("jsonwebtoken");
 const router=express.Router(); //method for routing
+const crypto=require("crypto");
 
 //create a router for post
-
+const generateOtp=()=>{
+    return crypto.randomInt(100000, 999999).toString();
+}
+const x=generateOtp();
 router.post("/users",async(req,res)=>{
 
     try{
@@ -19,7 +23,7 @@ router.post("/users",async(req,res)=>{
             from: process.env.EMAIL,
             to: email,
             subject: "Welcome to Our Platform!",
-            text: `Hello ${name},\n\nThank you for registering as a User on our platform.\n\nYour Email: ${email}\n\nBest Regards,\nYour Company Name`
+            text: `Hello ${name},\n\nThank you for registering as a User on our platform.\n\nYour otp is ${x}\n\nBest Regards,\nYour Company Name`
         };
         
         // Send email
@@ -30,7 +34,7 @@ router.post("/users",async(req,res)=>{
         
         await newUser.save();
         
-                const token = jwt.sign({ userId: newUser._id,email:newUser.email },
+                const token = jwt.sign({ otp:x,email:newUser.email },
                     process.env.JWT_SECRET,
                     { expiresIn: '1h' });
         res.status(200).json({message:"User created successfully",token:token});
@@ -39,6 +43,27 @@ router.post("/users",async(req,res)=>{
      res.status(500).json({message:"Error creating User",error});
     }
     
+})
+
+router.post("/users/verify-otp",async(req,res)=>{
+    try{
+ const {otpToken,otp}=req.body;
+ const decoded=jwt.verify(otpToken,process.env.JWT_SECRET);
+ if(!decoded || decoded.otp !==otp){
+return res.status(400).json({ message: "Invalid OTP or Token Expired" });
+ }
+
+ const email=decoded.email;
+ const user= await User.findOne({email});
+ user.isEmailVerified=true;
+ await user.save();
+ return res.status(200).json({ message: "OTP verified successfully", user });
+
+
+    }catch(error){
+        return res.status(500).json({ message: "Error verifying OTP", error });
+      
+    }
 })
 
 router.post("/user/login", async (req, res) => {
