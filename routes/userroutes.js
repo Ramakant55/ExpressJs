db4f4e9bc3cf521eee66ce6da5e2872884df276b
Ramakant55 <jangirramakant786@gmail.com>
2025-02-19 20:56:10 +0530

    get close

--- routes/userroutes.js
+++ routes/userroutes.js
@@ -169,26 +169,26 @@ router.post("/user/login", async (req, res) => {
 });
 
 
-router.get("/allusers", authenticateToken, async(req,res)=>{
-    try{
-        const users=await User.find();
-        res.status(200).json({users});
-    }catch(error){
-        res.status(500).json({message:"Error fetching Users",error});
-    }
-})
+// router.get("/allusers", authenticateToken, async(req,res)=>{
+//     try{
+//         const users=await User.find();
+//         res.status(200).json({users});
+//     }catch(error){
+//         res.status(500).json({message:"Error fetching Users",error});
+//     }
+// })
 
-router.get("/users/:id", authenticateToken, async(req,res)=>{
-    try{
-        const user=await User.findById(req.params.id);
-        if(!user){
-            return res.status(404).json({message:"User not found"});
-        }
-        res.status(200).json( user);
-    }catch(error){
-        res.status(500).json({message:"Error fetching User",error});
-    }
-})
+// router.get("/users/:id", authenticateToken, async(req,res)=>{
+//     try{
+//         const user=await User.findById(req.params.id);
+//         if(!user){
+//             return res.status(404).json({message:"User not found"});
+//         }
+//         res.status(200).json( user);
+//     }catch(error){
+//         res.status(500).json({message:"Error fetching User",error});
+//     }
+// })
 //create a router for put
 router.put("/users/:id", authenticateToken, async(req,res)=>{
     try{
